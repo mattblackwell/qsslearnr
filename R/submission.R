9ac7d995a110c1ca5e9de53a533f2a5ee3b0a279
Matt Blackwell <mblackwell@gov.harvard.edu>
2020-09-07 14:13:49 -0400

    added HTML backup download, better parsing of skips vs complete

--- R/submission.R
+++ R/submission.R
@@ -21,42 +21,56 @@ submission_server <- function(input, output) {
 
   # Evaluate in parent frame to get input, output, and session
   local({
-    output$download_report <- downloadHandler(
-      # For PDF output, change this to "report.pdf"
+    build_report <- function(file) {
+      # Copy the report file to a temporary directory before processing it, in
+      # case we don't have write permissions to the current working dir (which
+      # can happen when deployed).
+      tempReport <- file.path(tempdir(), "tutorial-report.Rmd")
+      file.copy("../tutorial-report.Rmd", tempReport, overwrite = TRUE)
+      
+      
+      # Set up parameters to pass to Rmd document
+      objs <- learnr:::get_all_state_objects(session)
+      skips <- learnr:::section_skipped_progress_from_state_objects(objs)
+      subs <- learnr:::submissions_from_state_objects(objs)
+      ##browser()
+      out <- tibble::tibble(
+        id = purrr::map_chr(subs, "id"),
+        answers = purrr::map_chr(subs, list("data", "answer"),
+                                 .default = NA),
+        checked = purrr::map_lgl(subs, list("data", "checked"),
+                                 .default = NA),
+        correct = purrr::map_lgl(subs, list("data", "feedback", "correct"),
+                                 .default = NA)
+      )
+      out$attempted <- !is.na(out$answers) | out$checked
+      params <- list(reporttitle = tut_reptitle,
+                     output = out,
+                     student_name = input$name,
+                     skipped = length(skips))
+      
+      ext <- tools::file_ext(file)
+      out_format <- switch(ext, pdf = "pdf_document", html = "html_document")
+
+      # Knit the document, passing in the `params` list, and eval it in a
+      # child of the global environment (this isolates the code in the document
+      # from the code in this app).
+      rmarkdown::render(
+        tempReport,
+        output_format = out_format,
+        output_file = file,
+        params = params,
+        envir = new.env(parent = globalenv())
+      )
+    }
+    output$download_pdf <- downloadHandler(
       filename = "report.pdf",
-      content = function(file) {
-        # Copy the report file to a temporary directory before processing it, in
-        # case we don't have write permissions to the current working dir (which
-        # can happen when deployed).
-        tempReport <- file.path(tempdir(), "tutorial-report.Rmd")
-        file.copy("../tutorial-report.Rmd", tempReport, overwrite = TRUE)
-
-        
-        # Set up parameters to pass to Rmd document
-        objs = learnr:::get_all_state_objects(session)
-        objs = learnr:::submissions_from_state_objects(objs)
-        out <- tibble::tibble(
-          id = purrr::map_chr(objs, "id"),
-          answers = purrr::map_chr(objs, list("data", "answer"),
-                                    .default = NA),
-          checked = purrr::map_lgl(objs, list("data", "checked"),
-                                   .default = NA),
-          correct = purrr::map_lgl(objs, list("data", "feedback", "correct"),
-                                   .default = NA)
-        )
-        out$attempted <- !is.na(out$answers) | out$checked
-        params <- list(reporttitle = tut_reptitle,
-                       output = out,
-                       student_name = input$name)
-
-        # Knit the document, passing in the `params` list, and eval it in a
-        # child of the global environment (this isolates the code in the document
-        # from the code in this app).
-        rmarkdown::render(tempReport, output_file = file,
-          params = params,
-          envir = new.env(parent = globalenv())
-          )
-      }
+      content = build_report
+    )
+    
+    output$download_html <- downloadHandler(
+      filename = "report.html",
+      content = build_report
     )
   }, envir = p)
 }
@@ -98,7 +112,8 @@ submission_ui <- shiny::div(
     shiny::tags$li("Click the Download button next to generate a report PDF with a summary of your work. "),
     shiny::tags$li("Upload this file to the appropriate assignment on Gradescope.")),
   shiny::textInput("name", "Your Name"),
-  shiny::downloadButton(outputId = "download_report", label = "Download")
+  shiny::downloadButton(outputId = "download_pdf", label = "Download PDF"),
+  shiny::downloadButton(outputId = "download_html", label = "Download HTML (backup)")
 )
 
 utils::globalVariables(c("input", "session", "download_report"))
